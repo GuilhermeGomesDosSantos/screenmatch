330114521a87bd18e6df4fb0a2ed66f1a777e8f4
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-19 19:26:25 -0300

    Desafio 1 - classe Pessoa com um método que exibe "Olá, mundo!"

---
+++ src/Desafios/Pessoa/Pessoa.java
@@ -0,0 +1,11 @@
+/*
+Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
+*/
+
+package Desafios.Pessoa;
+
+public class Pessoa {
+    void saudacao() {
+        System.out.println("Olá, mundo!");
+    }
+}
---
+++ src/Desafios/Pessoa/main.java
@@ -0,0 +1,9 @@
+package Desafios.Pessoa;
+
+public class main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.saudacao();
+    }
+}
