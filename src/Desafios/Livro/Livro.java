be8d24d54de11e54667b73ed1e034e091d8399b6
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-20 23:33:30 -0300

    Desafio - Livro

---
+++ src/Desafios/Livro/Livro.java
@@ -0,0 +1,33 @@
+/*
+Desenvolva uma classe Livro com os atributos privados titulo e autor.
+Utilize métodos getters e setters para acessar e modificar esses atributos.
+Adicione um método exibirDetalhes que imprime o título e o autor do livro.
+*/
+
+package Desafios.Livro;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    void exibirDetalhes() {
+        System.out.println("Titulo do Livro: " + getTitulo());
+        System.out.println("Nome do Autor do livro " + getTitulo() + " - Autor: " + getAutor());
+    }
+}
---
+++ src/Desafios/Livro/Main.java
@@ -0,0 +1,11 @@
+package Desafios.Livro;
+
+public class Main {
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+
+        livro.setTitulo("POO JAVA");
+        livro.setAutor("Guilherme");
+        livro.exibirDetalhes();
+    }
+}
