eb88b3c8d6ebea41a15f3eda2ef6c9b7f654380c
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-21 21:25:04 -0300

    Incluindo comportamentos adicionais

---
+++ src/br/com/alura/screenmatch/calculo/Classificavel.java
@@ -0,0 +1,7 @@
+package br.com.alura.screenmatch.calculo;
+
+public interface Classificavel {
+    // a interface é um conjunto de métodos abstrados, são metodos sem implementação
+    // apenas são declarados, sem possuir corpo de código
+    int getClassificacao();
+}
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,11 +1,14 @@
 package br.com.alura.screenmatch.modelos;
 
+import br.com.alura.screenmatch.calculo.Classificavel;
+
 /*
 A classe especifica o conteúdo de um objeto
 quais caracteristicas o filme possui ?
 todas as variaveis que forem feitas, todos os filmes irão possuir
 */
-public class Filme extends Titulo{
+public class Filme extends Titulo implements Classificavel {
+    // Usa implements para usar uma interface
     /*
 A classe Filme extende da classe Titulo, o filme tem tudo que titulo tem mais o diretor
 o filme irá saber fazer tudo que titulo faz
@@ -18,5 +21,10 @@ o filme irá saber fazer tudo que titulo faz
 
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return 0;
     }
 }
