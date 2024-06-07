e6e24251e302015bcb3f1b83af57d721ebc9be8f
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-07 17:16:09 -0300

    Desafio 03 - Modifique o Exercício 2 para incluir uma verificação usando instanceof

--- src/Desafios_Listas/Animal/Main.java
+++ src/Desafios_Listas/Animal/Main.java
@@ -1,9 +1,16 @@
 package Desafios_Listas.Animal;
 
 public class Main {
+
     public static void main(String[] args) {
         Cachorro dog = new Cachorro("Dog 1");
 
+        if (dog instanceof Cachorro && dog.getNome() == "Dog 123") {
+            System.out.println("O nome é Dog 1");
+        } else {
+            System.out.println("É outro nome");
+        }
+//
         Animal animal = (Animal) dog;
         animal.fazendoAcao();
     }
