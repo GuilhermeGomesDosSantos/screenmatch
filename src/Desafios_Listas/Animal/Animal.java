3a0ccc64bf1ae85856e9118fc3e671cec1a2ec93
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-07 17:16:24 -0300

    Desafio 03 - Modifique o Exercício 2 para incluir uma verificação usando instanceof

--- src/Desafios_Listas/Animal/Animal.java
+++ src/Desafios_Listas/Animal/Animal.java
@@ -1,7 +1,13 @@
 /*
-Crie uma classe Animal e uma classe Cachorro que herda de Animal.
+1 - Crie uma classe Animal e uma classe Cachorro que herda de Animal.
 Em seguida, crie um objeto da classe Cachorro e faça o casting para a classe Animal.
- */
+
+2 - Crie uma classe Animal e uma classe Cachorro que herda de Animal. Em seguida,
+crie um objeto da classe Cachorro e faça o casting para a classe Animal.
+
+3 - Modifique o Exercício 2 para incluir uma verificação usando instanceof
+para garantir que o objeto seja do tipo correto antes de fazer o casting.
+*/
 
 package Desafios_Listas.Animal;
 
